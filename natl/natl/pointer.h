0a71da4e04512945f0dc5b0d5cbedd24511b3644
NovoAffectDev <112781372+NovoAffectDev@users.noreply.github.com>
2024-02-07 21:18:40 -0500

    added ObserverPtr

--- natl/natl/pointer.h
+++ natl/natl/pointer.h
@@ -1365,4 +1365,83 @@ namespace natl {
 
 	template<class DataType>
 	using WeakPtr = typename SharedPtr<DataType>::weak_type;
+
+	template<class DataType>
+	class ObserverPtr {
+	public:
+		using element_type = DataType;
+	private:
+		element_type* dataPtr;
+	public:
+		//construtor
+		constexpr ObserverPtr() noexcept = default;
+		constexpr ObserverPtr(const ObserverPtr&) noexcept = default;
+		constexpr ObserverPtr(ObserverPtr&&) noexcept = default;
+		constexpr explicit ObserverPtr(element_type* dataPtr) noexcept : dataPtr(dataPtr) {}
+
+		//destructor
+		constexpr ~ObserverPtr() noexcept = default;
+
+		//util 
+		constexpr ObserverPtr& self() noexcept { return *this; }
+		constexpr const ObserverPtr& self() const noexcept { return *this; }
+
+		//assignment 
+		constexpr ObserverPtr& operator=(const ObserverPtr&) noexcept = default;
+		constexpr ObserverPtr& operator=(ObserverPtr&&) noexcept = default;
+
+		//modifiers
+		[[nodiscard]] constexpr element_type* release() noexcept {
+			element_type* tempDataPtr = dataPtr;
+			dataPtr = nullptr;
+			return dataPtr;
+		}
+		constexpr void reset(element_type* p = nullptr) noexcept {
+			dataPtr = p;
+		}
+		constexpr void swap(ObserverPtr& other) noexcept {
+			element_type* tempDataPtr = other.tempDataPtr;
+			other.dataPtr = dataPtr;
+			dataPtr = tempDataPtr;
+		}
+
+		//observers
+		constexpr element_type* get() const noexcept { return dataPtr; }
+		constexpr explicit operator bool() const noexcept { return dataPtr; }
+		constexpr element_type& operator*() const { return dataPtr; }
+		constexpr element_type* operator->() const noexcept { return dataPtr; }
+		constexpr bool isNull() noexcept { return dataPtr == nullptr; }
+		constexpr bool isNotNull() noexcept { return dataPtr != nullptr; }
+
+		//conversions
+		constexpr explicit operator element_type* () const noexcept { return dataPtr; }
+
+		//compare 
+		constexpr bool operator==(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr == other.dataPtr;
+		}
+		constexpr bool operator!=(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr != other.dataPtr;
+		}
+		constexpr bool operator<(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr < other.dataPtr;
+		}
+		constexpr bool operator>(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr > other.dataPtr;
+		}
+		constexpr bool operator<=(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr <= other.dataPtr;
+		}
+		constexpr bool operator>=(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr >= other.dataPtr;
+		}
+		constexpr auto operator<=>(const ObserverPtr<DataType>& other) noexcept {
+			return dataPtr <=> other.dataPtr;
+		}
+	};
+
+	template<class DataType>
+	constexpr ObserverPtr<DataType> makeObserver(DataType* ptr) noexcept {
+		return ObserverPtr<DataType>(ptr);
+	}
 }
